e0313d5c28fc3cc8180387e6cd40f91d7bee958c
emanuelbap <emanuelapolinario99@gmail.com>
2026-03-25 10:48:18 -0300

    update payment api for postgres

--- app/settings.py
+++ app/settings.py
@@ -7,7 +7,7 @@ import os
 class Settings:
     database_url: str = os.getenv(
         "DATABASE_URL",
-        "postgresql+psycopg2://postgres:postgres@db:5432/pagamentos",
+        "postgresql+psycopg2://postgres:98765@db:5432/pagamentos",
     )
     users_api_url: str = os.getenv("USERS_API_URL", "http://18.228.48.67/users")
     users_api_timeout: float = float(os.getenv("USERS_API_TIMEOUT", "5"))
